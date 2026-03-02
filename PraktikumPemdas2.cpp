aa2afd93f3bf2adf7f387cebebbd4b109437ffcb
envikaaa <envikapriyatna31@gmail.com>
2026-03-02 15:34:06 +0700

    float luasLingkaran

--- PraktikumPemdas2.cpp
+++ PraktikumPemdas2.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 //deklarasi variabel global
 int r;
-float phi = 3.14159;
 
 //implementasi fungsi dan prosedur
 void input(){
     cout << "masukkan r : ";
     cin >> r;
 }
-int luasLingkaran (float a, float b)
+float luasLingkaran (int b){
+    return phi*b*b;
+}
